e37b437b637360a6ee15e891fb3537d434df2418
ans <497g0100@stust.edu.tw>
2017-12-22 04:07:37 +0800

    test2
    無法讀取檔案清單 會閃退

--- app/src/main/java/org/phisten/appaccountswitch/MainActivity.kt
+++ app/src/main/java/org/phisten/appaccountswitch/MainActivity.kt
@@ -1,9 +1,19 @@
 package org.phisten.appaccountswitch
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.os.Environment
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,8 +39,67 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //ListView lv = () R.id.list
+        val lv : ListView = findViewById(R.id.list)
+        lv.adapter = ListExampleAdapter(this)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+
+    private class ListExampleAdapter(context: Context) : BaseAdapter() {
+
+        val filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
+        val file = File(filePath)
+        val files = file.listFiles()
+        val filesName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).list()
+
+        //var filelist = files.toList().toString();
+        //internal var sList = filesName
+
+        internal var sList =  arrayOf(filePath,  "a2:" +  filesName[0] , "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen")
+
+        private val mInflator: LayoutInflater
+
+        init {
+            this.mInflator = LayoutInflater.from(context)
+        }
+
+        override fun getCount(): Int {
+            return sList.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return sList[position]
+
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+            val view: View?
+            val vh: ListRowHolder
+            if (convertView == null) {
+                view = this.mInflator.inflate(R.layout.list_row, parent, false)
+                vh = ListRowHolder(view)
+                view.tag = vh
+            } else {
+                view = convertView
+                vh = view.tag as ListRowHolder
+            }
+
+            vh.label.text = sList[position]
+//            vh.label.text = sList[position].name
+            return view
+        }
+    }
+
+    private class ListRowHolder(row: View?) {
+        public val label: TextView
+
+        init {
+            this.label = row?.findViewById<TextView>(R.id.label) as TextView
+        }
     }
 }
